742d45e9526eb469bae49f823339212fd0d38030
coderHsc <18386979@qq.com>
2015-03-17 17:38:03 +0800

    Update td_xml.lua

--- td_xml.lua
+++ td_xml.lua
@@ -11,60 +11,56 @@ for line in xmlFile:lines() do
 
 
 	if len > 0 then 
-		local start = 1
-		local aEnd=string.find(line," ",start)
-		local a = string .sub(line,start,aEnd-1)
-		start = aEnd+1 
+		local num = string.sub(line,1,1)
+		local tonum= tonumber(num)
 
-		local bEnd=string.find(line," ",start)
-		local b = string .sub(line,start,bEnd-1)
-		start = bEnd+1
+		if tonum~=nil then
+			local start = 1
+			local aEnd=string.find(line," ",start)
+			local a = string .sub(line,start,aEnd-1)
+			start = aEnd+1 
 
-		local cEnd=string.find(line," ",start)
-		if cEnd == nil then 
-			break 
+			local bEnd=string.find(line," ",start)
+			local b = string .sub(line,start,bEnd-1)
+			start = bEnd+1
+
+			local cEnd=string.find(line," ",start)
+			if cEnd ~= nil then 
+			
+				local c = string .sub(line,start,cEnd-1)
+
+				start = cEnd+1
+
+				local dEnd=string.find(line," ",start)
+				local d
+				local e 
+				if dEnd ~= nil then 
+				    d =string.sub(line,start,dEnd-1)
+					start = dEnd+1
+					local eEnd=string.find(line," ",start)
+					e =string.sub(line,start,eEnd-1)
+				else
+					d =string.sub(line,start)
+				end
+				if e ~= nil then 
+					cnt = cnt +1
+					print("<key>image"..cnt..".png</key>")
+					print("<dict>")
+					print("<key>frame</key>")
+					print("<string>{{" .. b .. "," .. c .."},{" ..d.. "," .. e.. "}}</string>")
+					print("<key>offset</key>")
+					print("<string>{0,0}</string>")
+					print("<key>rotated</key>")
+					print("<false/>")
+					print("<key>sourceColorRect</key>")
+					print("<string>{{0,0},{" ..d.. "," .. e.. "}}</string>")
+					print("<key>sourceSize</key>")
+					print("<string>{" ..d.. "," .. e.. "}}</string>")
+					print("</dict>")
+				end 
+			end
 		end
-		local c = string .sub(line,start,cEnd-1)
-
-		start = cEnd+1
-
-		local dEnd=string.find(line," ",start)
-		local d
-		local e 
-		if dEnd ~= nil then 
-		    d =string.sub(line,start,dEnd-1)
-			start = dEnd+1
-			local eEnd=string.find(line," ",start)
-			e =string.sub(line,start,eEnd-1)
-		else
-			d =string.sub(line,start)
-		end
-		if e ~= nil then 
-			cnt = cnt +1
-		print("<key>image"..cnt..".png</key>")
-		print("<dict>")
-		print("<key>frame</key>")
-		print("<string>{{" .. b .. "," .. c .."},{" ..d.. "," .. e.. "}}</string>")
-		print("<key>offset</key>")
-		print("<string>{0,0}</string>")
-		print("<key>rotated</key>")
-		print("<false/>")
-		print("<key>sourceColorRect</key>")
-		print("<string>{{0,0},{" ..d.. "," .. e.. "}}</string>")
-		print("<key>sourceSize</key>")
-		print("<string>{" ..d.. "," .. e.. "}}</string>")
-		print("</dict>")
-
-
-
-
-		end
-
-
-		--print("a,b,c,d",a,b,c,d,e)
--- 	return a,b,c,d
 	end 
-	--print("",len,line)
 end
 
 -- function getABCD(str)
